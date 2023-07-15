82264c15bd411ec5c417eb42340481293b8b8170
minnie <137783738+minnieoo@users.noreply.github.com>
2023-07-15 14:29:48 -0500

    Update text-resize.user.js

--- text-resize.user.js
+++ text-resize.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Change font size
 // @namespace    https://github.com/aclist
-// @version      0.3.9
+// @version      0.4.0
 // @description  Change the size of comment text.
 // @author       minnieo
 // @match        https://kbin.social/*
